a34da6923684e074da5cd4159921c55f67e8a2c5
Spencer Kline <dskline1@gmail.com>
2020-06-02 09:57:11 -0400

    feat(a11y): add <noscript> tag for when javascript is disabled

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Icon as ChakraIcon } from '@chakra-ui/core'
+
+const Icon: typeof ChakraIcon = (props) => (
+  <ChakraIcon {...props} />
+)
+
+export default Icon
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import React, { FC, useEffect } from 'react'
+import { BsQuestionCircle } from 'react-icons/bs'
 import { AppProps } from 'next/app'
 
+import Box from 'src/components/_layout/Box'
+import Link from 'src/components/Link'
+import NoScript from 'src/components/NoScript'
 import * as analytics from 'src/utilities/analytics'
 
 export function reportWebVitals (metrics): void {
@@ -8,13 +12,34 @@ export function reportWebVitals (metrics): void {
 }
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-
   useEffect(() => {
     analytics.init()
   })
 
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <NoScript>
+        <Box
+          position='fixed'
+          bottom={0}
+          padding={1}
+          width='100vw'
+          textAlign='center'
+        >
+          <Link
+            href='https://www.enable-javascript.com/'
+            isExternal
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+          >
+            <Box as={BsQuestionCircle} display='inline' marginRight={2} />
+            Enable JavaScript for the best experience
+          </Link>
+        </Box>
+      </NoScript>
+    </>
   )
 }
 
