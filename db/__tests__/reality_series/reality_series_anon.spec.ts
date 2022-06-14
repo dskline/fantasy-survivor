4a00257136ab883d9cad80a818e7b4934e5f8dce
Spencer Kline <dskline1@gmail.com>
2022-06-14 13:53:08 -0400

    fix(test): error object is still null select policy

--- db/__tests__/reality_series/reality_series_anon.spec.ts
+++ db/__tests__/reality_series/reality_series_anon.spec.ts
@@ -1,18 +1,32 @@
-import { beforeAll, describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import dbClient from "@/__tests__/dbClient";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 describe("anon access on reality_series table", () => {
   let supabase: SupabaseClient;
+  let testSlug: "testId";
 
   beforeAll(async () => {
+    const seeder = await dbClient("service");
+    seeder.from("reality_series").insert({
+      slug: testSlug,
+      title: "test",
+      in_progress: false,
+      logo_src: "logo.png",
+    });
+
     // should work for any user role except service role
     supabase = await dbClient("anon");
   });
 
-  it("should not be readable", async () => {
-    let { error } = await supabase.from("reality_series").select("id");
-    expect(error).toBeDefined();
+  afterAll(async () => {
+    const cleanup = await dbClient("service");
+    cleanup.from("reality_series").delete().eq("slug", testSlug);
+  });
+
+  it("should be readable", async () => {
+    let { data } = await supabase.from("reality_series").select("slug");
+    expect(data?.length).toBeGreaterThan(0);
   });
 
   it("should not be writable", async () => {
