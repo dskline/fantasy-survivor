fca9dc0d9cf3a11a8c126e5c3347cc0b87341f9e
Spencer Kline <dskline1@gmail.com>
2022-08-04 10:48:38 -0400

    feature(db): add league_formats and some missing RLS policies

--- db/seed/index.ts
+++ db/seed/index.ts
@@ -1,5 +1,9 @@
 import { seedSurvivor } from "./survivor/survivor.seed";
+import { dbClient } from '@/seed/dbClient'
 
 export const seedData = async () => {
+  const supabase = await dbClient();
+  supabase.from("graphql.rebuildSchema()");
+
   await seedSurvivor();
 };
