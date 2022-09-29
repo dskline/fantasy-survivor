ef96bf7b8229e7da962e43aa32176d2948862a1e
Spencer Kline <dskline1@gmail.com>
2022-09-29 10:21:04 -0400

    bugfix: forgot to commit this file

--- webapp/features/core/leagues/LeaguePage/useLeagueUser.ts
+++ webapp/features/core/leagues/LeaguePage/useLeagueUser.ts
@@ -41,5 +41,6 @@ export const useLeagueUser = (league: LeagueProps): LeagueUser => {
         isLoading: isLoading || fetching,
         userRoster: {} as UserRoster,
         features: [],
+        watched: [],
       };
 };
