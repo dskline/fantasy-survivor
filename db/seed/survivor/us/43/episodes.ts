e964aedfbb5a214d6888240157522d3ccfd65e19
Spencer Kline <dskline1@gmail.com>
2022-10-13 20:38:21 -0400

    chore: seed episode 4

--- db/seed/survivor/us/43/episodes.ts
+++ db/seed/survivor/us/43/episodes.ts
@@ -3,6 +3,7 @@ import { createSeasonSeeder } from "@/seed/survivor/seeder/createSeasonSeeder";
 import { episode1 } from "@/seed/survivor/us/43/episodes/survivor-us-43-01";
 import { episode2 } from "@/seed/survivor/us/43/episodes/survivor-us-43-02";
 import { episode3 } from "@/seed/survivor/us/43/episodes/survivor-us-43-03";
+import { episode4 } from '@/seed/survivor/us/43/episodes/survivor-us-43-04'
 
 export async function survivor43() {
   const seeder = await createSeasonSeeder(
@@ -13,4 +14,5 @@ export async function survivor43() {
   await seeder.recordEpisode(episode1);
   await seeder.recordEpisode(episode2);
   await seeder.recordEpisode(episode3);
+  await seeder.recordEpisode(episode4);
 }
---
+++ db/seed/survivor/us/43/episodes/survivor-us-43-04.ts
@@ -0,0 +1,36 @@
+import { Contestants, tribes } from '@/seed/survivor/us/43/tribes'
+import { SurvivorEpisode } from '@/seed/survivor/seeder/SeasonSeeder'
+
+export const episode4: SurvivorEpisode = {
+  startTime: new Date("Oct 12 2022 19:00:00 EST"),
+  events: [
+    {
+      rule: "ChallengeWin",
+      players: tribes.Vesi.contestants,
+    },
+    {
+      rule: "ChallengeWin",
+      players: tribes.Vesi.contestants,
+    },
+    {
+      rule: "ChallengeRunnerUp",
+      players: tribes.Baka.contestants,
+    },
+    {
+      rule: "SitOut",
+      players: [Contestants.ELIE, Contestants.JAMES, Contestants.LINDSAY],
+    },
+    {
+      rule: "SitOut",
+      players: [Contestants.JEANINE, Contestants.GEO, Contestants.CASSIDY],
+    },
+    {
+      rule: "VoteTarget",
+      players: [Contestants.CASSIDY, Contestants.GEO, Contestants.LINDSAY],
+    },
+    {
+      rule: "VotedOut",
+      players: [Contestants.LINDSAY],
+    },
+  ],
+};
